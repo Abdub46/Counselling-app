07841f3b03da36aac7991e1bcd8de1d91f76f7ee
Abdub46 <abdubgalgallo46@gmail.com>
2026-07-23 14:31:10 +0300

    Revert "cron"
    This reverts commit 1504d8fb2d9f14c53ebb66d27b67c52dd26c9800.

--- backend/routes/cronRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { runSoldReminders } = require('../controllers/cronController');
-const cronAuth = require('../middleware/cronAuth');
-
-router.get('/sold-reminders', cronAuth, runSoldReminders);
-
-module.exports = router;
